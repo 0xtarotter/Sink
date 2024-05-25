80d74028b8a01da2b8eabace7bbb0647aab46436
TΛRØTTΞR <399929@gmail.com>
2024-05-26 02:22:05 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -66,10 +66,13 @@ const showMenu = ref(false)
                 target="_blank"
                 class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
               >
-                <GitHubIcon
-                  class="w-5 h-5 mr-1"
-                />
-                IP info</a>
+                IPinfo</a>
+                    <a
+                href="https://cesu.wang"
+                target="_blank"
+                class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
+              >
+                Speedtest</a>
             </div>
           </div>
         </div>
