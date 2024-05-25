2fc1181864c67a2cbdd515be54e6dac333533f93
TΛRØTTΞR <399929@gmail.com>
2024-05-26 01:35:30 +0800

    Update Cta.vue

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -3,17 +3,17 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      Deployment immediately.
+      Googa.
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
-      With just a few simple clicks, you can start deploying without any expenses.
+      A Simple / Speedy / Secrue Link Shortener with Analytics, 100% run on Cloudflare.
     </p>
     <div class="flex mt-5">
       <HomeLink
-        href="https://github.com/ccbikai/sink?tab=readme-ov-file#%EF%B8%8F-deployment"
+        href="/"
         type="inverted"
       >
-        Start Deploy
+        Start
       </HomeLink>
     </div>
   </div>
