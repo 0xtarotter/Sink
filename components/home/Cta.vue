c7bf107a34b6e0fe9b79817a346af7006d31c8fe
TΛRØTTΞR <399929@gmail.com>
2024-05-26 01:39:53 +0800

    Update Cta.vue

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -3,14 +3,14 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      Googa.
+      Googa
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
       A Simple / Speedy / Secrue Link Shortener with Analytics, 100% run on Cloudflare.
     </p>
     <div class="flex mt-5">
       <HomeLink
-        href="/"
+        href="/dashboard"
         type="inverted"
       >
         Start
